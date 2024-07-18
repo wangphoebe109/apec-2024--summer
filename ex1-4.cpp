28f2eefa1df222d254c209313ea26567015ba5fa
Cloud Girl <138782811+wangphoebe109@users.noreply.github.com>
2024-07-18 20:42:53 +0800

    Create ex1-4.cpp
    This is my forth code.

---
+++ ex1-4.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+using namespace std;
+
+int main(void)
+{
+    printf("the building is %d m %d cm \n",1,80);
+    printf("the building is %8d cm \n",49730);
+    printf("the building is %8d cm \n",3975);
+    printf("the building is %d cm \n",29579328);
+    printf("pi = %03.11f \n",3.1415926535897932384626433832795);
+}
