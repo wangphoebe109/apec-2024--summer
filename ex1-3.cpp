bda8ed7ecd896bcb6a1b0f3693ff02fb1c9bbe19
Cloud Girl <138782811+wangphoebe109@users.noreply.github.com>
2024-07-18 20:27:41 +0800

    Create ex1-3.cpp
    This is my third code.

---
+++ ex1-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+       
+    cout <<"hello Selena" << endl;
+    cout <<"my height is " << 1 << " m " << 68 << " cm " << endl;
+    cout <<"my height is 1 m 68 cm " <<endl;
+    cout <<"my height is 1.68 m " <<endl;
+
+    return 0;
+}
