9930cf92365b79683fcf0d06ce8f694a68e5f8fd
Cloud Girl <138782811+wangphoebe109@users.noreply.github.com>
2024-07-18 20:05:25 +0800

    Update ex.1-2.cpp

--- ex.1-2.cpp
+++ ex.1-2.cpp
@@ -4,4 +4,6 @@ int main(void)
 {
     printf("hello Selean \n");
     printf("my height is %d m %d cm \n",1,68);
+    printf("my height is %5d m %5d cm \n",1,68);
+    printf("my height is %d 5 m %d 5 cm \n",1,68);
 }
