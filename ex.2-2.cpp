5ec2402e6e565868fdbd9475cd3be474e0ea848a
Cloud Girl <138782811+wangphoebe109@users.noreply.github.com>
2024-08-01 19:26:33 +0800

    Create ex.2-2.cpp

---
+++ ex.2-2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("A=> ");
+    scanf("%d",&a);
+    printf("B=> ");
+    scanf("%d", &b);
+    printf("A+B=%d\n", a+b);
+
+    return 0;
+}
