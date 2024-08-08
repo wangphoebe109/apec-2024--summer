3ff1308ecb0f49dad2ef880ae345b0d065ae686e
Cloud Girl <138782811+wangphoebe109@users.noreply.github.com>
2024-08-08 19:09:39 +0800

    Create ex.3-1.cpp

---
+++ ex.3-1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x,y;
+    int bool_value;
+    
+    x = 3 ; x = 4;
+    
+    bool_value = (x > y);
+    printf("x > y ==> %d \n",bool_value);
+    bool_value = (x < y);
+    printf("X < y ==> %d \n",bool_value);
+
+}
