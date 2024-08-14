e9587b14856e64f3ea4ca4e4133aab84014f86d5
Cloud Girl <138782811+wangphoebe109@users.noreply.github.com>
2024-08-14 22:47:33 +0800

    Create homework.3-6.cpp

---
+++ homework.3-6.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+ //從鍵盤上輸入10個數，求其平均值。
+
+int main()
+{
+    int avg,all;
+    for(int i=0;i<10;i++){
+        int a[i];
+        printf("%d.輸入數字: ",i+1);
+        scanf("%d",&a[i]);
+        all += a[i];
+    }
+    avg = all / 10;
+    printf("%d",avg);
+    
+    return 0;
+}
